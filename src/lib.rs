4dbed7d2f4ea2f4014cb579458f119cee4f8e4ab
Keshav Priyadarshi <git@keshav.space>
2025-11-11 19:14:04 +0530

    Load FST using memory-mapped file
    Signed-off-by: Keshav Priyadarshi <git@keshav.space>

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
 use fst::Set;
+use memmap2::Mmap;
 use once_cell::sync::Lazy;
+use std::env;
+use std::fs::File;
+use std::path::Path;
 
-static FST_BYTES: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/purls.fst"));
-
-static VALIDATOR: Lazy<Set<&[u8]>> =
-    Lazy::new(|| Set::new(FST_BYTES).expect("Failed to load FST from embedded bytes"));
+static VALIDATOR: Lazy<Set<Mmap>> = Lazy::new(|| {
+    let path = Path::new(env!("CARGO_MANIFEST_DIR")).join("purls.fst");
+    let file = File::open(path).expect("Failed to open FST file");
+    let mmap = unsafe { Mmap::map(&file).expect("Failed to mmap FST file") };
+    Set::new(mmap).expect("Failed to load FST from mmap")
+});
 
 pub fn validate(packageurl: &str) -> bool {
     let trimmed_packageurl = packageurl.trim_end_matches("/");
